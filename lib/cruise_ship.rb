7bfe06bff0b932f9567605772dced2dca2bbe346
Zachary Lehmann <zlehmann@gmail.com>
2018-09-20 00:37:35 +0000

    automatically backed up by learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,8 +1,8 @@
 # passengers = {
-# suite_a: "Amanda Presley", 
-# suite_b: "Seymour Hoffman", 
-# suite_c: "Alfred Tennyson", 
-# suite_d: "Charlie Chaplin", 
+# suite_a: "Amanda Presley",
+# suite_b: "Seymour Hoffman",
+# suite_c: "Alfred Tennyson",
+# suite_d: "Charlie Chaplin",
 # suite_e: "Crumpet the Elf"
 # }
 
@@ -14,6 +14,6 @@ def select_winner(passengers)
       winner = name
     end
   end
-   
+
   winner
 end
